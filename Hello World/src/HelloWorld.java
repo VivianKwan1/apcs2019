4dfd59e6a941c8f16753dfd75d7941435a999e1c
APCS3rd <APCS3rd@MHS-LT9-08>
2019-08-20 10:12:16 -0700

    complete Hello World project

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,13 @@
+//Vivian Kwan 
+//August 16, 2019
+//First class for APCS A
+
+//prints a message to the console
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
